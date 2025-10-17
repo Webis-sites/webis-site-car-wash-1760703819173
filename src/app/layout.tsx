f144093e0a292a85404e0f777a9872795e692544
Anass <146187264+nasaou@users.noreply.github.com>
2025-10-17 13:50:41 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Car wash',
+  description: 'Minimal ',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#3b82f6" />
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          {/* Navbar will be inserted here */}
+
+          {children}
+
+        </div>
+      </body>
+    </html>
+  );
+}
